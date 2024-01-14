927731e00350d7e0afff58abc1614ce2ecc4c99c
viniciusaze <152533810+viniciusaze@users.noreply.github.com>
2024-01-14 17:37:27 -0300

    atualizei minhas habilidades

---
+++ habilidades.js
@@ -0,0 +1,32 @@
+function html5(){
+
+    let text = document.getElementById('deshabhtml')
+
+    if (text.style.display == 'block'){
+        text.style.display ='none'
+    } else {
+        text.style.display = 'block'
+    }
+}
+
+function css(){
+
+    let text = document.getElementById('deshabcss')
+
+    if (text.style.display == 'block'){
+        text.style.display ='none'
+    } else {
+        text.style.display = 'block'
+    }
+}
+
+function js(){
+
+    let text = document.getElementById('deshabjs')
+
+    if (text.style.display == 'block'){
+        text.style.display ='none'
+    } else {
+        text.style.display = 'block'
+    }
+}
