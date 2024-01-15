ed1ffa26e32a717e763755285bc0f5600efd9bb4
viniciusaze <152533810+viniciusaze@users.noreply.github.com>
2024-01-15 15:57:43 -0300

    atualizado script habilidades

--- habilidades.js
+++ habilidades.js
@@ -1,32 +1,78 @@
+let texthtml = document.getElementById('deshabhtml')
+let textcss = document.getElementById('deshabcss')
+let textjs = document.getElementById('deshabjs')
+let divhtml = document.getElementById('habhtml')
+let divcss = document.getElementById('habcss')
+let divjs = document.getElementById('habjs')
+
 function html5(){
 
-    let text = document.getElementById('deshabhtml')
-
-    if (text.style.display == 'block'){
-        text.style.display ='none'
+    if (texthtml.style.display == 'block'){
+        texthtml.style.display ='none'
+        divhtml.style.width = '90px'
+        divhtml.style.height = '90px'
     } else {
-        text.style.display = 'block'
+        texthtml.style.display = 'block'
+
+        texthtml.style.width = '420px'
+        divhtml.style.width = '420px'
+        divhtml.style.height = '300px'
+
+        textcss.style.display = 'none'
+        textjs.style.display = 'none'
+
+        divcss.style.width = '90px'
+        divcss.style.height = '90px'
+
+        divjs.style.width = '90px'
+        divjs.style.height = '90px'
     }
 }
 
 function css(){
 
-    let text = document.getElementById('deshabcss')
-
-    if (text.style.display == 'block'){
-        text.style.display ='none'
+    if (textcss.style.display == 'block'){
+        textcss.style.display ='none'
+        divcss.style.width = '90px'
+        divcss.style.height = '90px'
     } else {
-        text.style.display = 'block'
+        textcss.style.display = 'block'
+
+        textcss.style.width = '420px'
+        divcss.style.width = '420px'
+        divcss.style.height = '300px'
+
+        texthtml.style.display = 'none'
+        textjs.style.display = 'none'
+
+        divhtml.style.width = '90px'
+        divhtml.style.height = '90px'
+
+        divjs.style.width = '90px'
+        divjs.style.height = '90px'
     }
 }
 
 function js(){
 
-    let text = document.getElementById('deshabjs')
-
-    if (text.style.display == 'block'){
-        text.style.display ='none'
+    if (textjs.style.display == 'block'){
+        textjs.style.display ='none'
+        divjs.style.width = '90px'
+        divjs.style.height = '90px'
     } else {
-        text.style.display = 'block'
+        textjs.style.display = 'block'
+
+        divjs.style.width = '420px'
+        divjs.style.width = '420px'
+        divjs.style.height = '300px'
+
+        texthtml.style.display = 'none'
+        textcss.style.display = 'none'
+
+        divhtml.style.width = '90px'
+        divhtml.style.height = '90px'
+
+        divcss.style.width = '90px'
+        divcss.style.height = '90px'
     }
 }
