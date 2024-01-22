8c5f34f8fcfeb3fb9090950b0a1f36d2f1f730b8
viniciusaze <152533810+viniciusaze@users.noreply.github.com>
2024-01-22 17:25:31 -0300

    iniciei media tela

--- scripts/mais-projetos.js
+++ scripts/mais-projetos.js
@@ -1,12 +1,18 @@
 function mais() {
     let botao = document.getElementById('mostrar-mais')
-    let mais = document.getElementById('mais-projetos')
+    let ele0 = document.getElementsByClassName('mais')[0]
+    let ele1 = document.getElementsByClassName('mais')[1]
+    let ele2 = document.getElementsByClassName('mais')[2]
 
-    if (mais.style.display === 'block') {
-        mais.style.display = 'none'
+    if (ele0.style.display === 'block') {
+        ele0.style.display = 'none'
+        ele1.style.display = 'none'
+        ele2.style.display = 'none'
         botao.innerHTML = 'Mostrar Mais 🢃'
     } else {
-        mais.style.display = 'block'
+        ele0.style.display = 'block'
+        ele1.style.display = 'block'
+        ele2.style.display = 'block'
         botao.innerHTML = 'Mostrar Menos 🢁'
     }
 }
