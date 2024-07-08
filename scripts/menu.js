3b413291a1235ede380f2414387f7c761d378aff
Vinícius Azevedo <ar-vinicius@hotmail.com>
2024-07-08 15:52:43 -0300

    Mudança no layout do portfolio

--- scripts/menu.js
+++
@@ -1,9 +0,0 @@
-function menu(){
-    let menu = document.getElementById('hamburguer')
-    let itens = document.getElementById('itens')
-    if (itens.style.display == 'block'){
-        itens.style.display = 'none'
-    } else {
-        itens.style.display = 'block'
-    }
-}
